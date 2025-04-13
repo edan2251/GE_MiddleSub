08bc3b52fae935ae5bfc6cb614c4239254789d08
edan <molphone0328@gmail.com>
2025-04-13 21:53:14 +0900

    [First] 프로젝트 생성 및 이것저것

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 5;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private Animator pAni;
+    private bool isGrounded;
+
+    private bool isGiant = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (moveInput * moveSpeed, rb.velocity.y);
+
+
+        if (isGiant)
+        {
+            if (moveInput < 0)
+                transform.localScale = new Vector3(2f, 2f, 1f);
+            if (moveInput > 0)
+                transform.localScale = new Vector3(-2f, 2f, 1f);
+        }
+        else
+        {
+            if (moveInput < 0)
+                transform.localScale = new Vector3(1f, 1f, 1f);
+
+            if (moveInput > 0)
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("JumpAction");
+        }
+
+
+
+
+
+
+
+    }//.Update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<LevelObject>().MoveToNextLevel();
+        }
+
+        if (collision.CompareTag("Enemy"))
+        {
+            if (isGiant)
+                Destroy(collision.gameObject);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (collision.CompareTag("Item"))
+        {
+            isGiant = true;
+            Destroy(collision.gameObject);
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/TilemapDisabler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapDisabler : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<TilemapRenderer>().enabled = false;
+    }
+
+
+
+}
